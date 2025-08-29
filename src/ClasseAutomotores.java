8502cab745e4d690a8835f75e74bd040143480e0
Faelzxpr09 <rafaelpracidellisilva@gmail.com>
2025-08-29 09:38:56 -0300

    Criado atributos, construtor vazio e um com todos atributos

--- src/ClasseAutomotores.java
+++ src/ClasseAutomotores.java
@@ -1,4 +1,24 @@
 
 public class ClasseAutomotores {
+	
+	String corAt;
+	String marcaAt;
+	String nomeAt;
+	String tipoAt;
 
+
+	//construtor vazio
+	public ClasseAutomotores() {
+		
+	}
+	
+	public ClasseAutomotores(String marcaPar, String nomePar, String tipoPar, String corPar) {
+		
+		this.corAt = corPar;
+		this.nomeAt = nomePar;
+		this.marcaAt = marcaPar;
+		this.corAt = corPar;
+		
+	}
 }
+
