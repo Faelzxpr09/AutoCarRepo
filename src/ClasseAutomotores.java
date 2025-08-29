b1e8e2e9d2d5feea4c7324ed2bd7eefa129ce266
Faelzxpr09 <rafaelpracidellisilva@gmail.com>
2025-08-29 09:48:20 -0300

    criado getters e setters para cada um

--- src/ClasseAutomotores.java
+++ src/ClasseAutomotores.java
@@ -2,8 +2,8 @@
 public class ClasseAutomotores {
 	
 	String corAt;
-	String marcaAt;
 	String nomeAt;
+	String marcaAt;
 	String tipoAt;
 
 
@@ -12,13 +12,38 @@ public class ClasseAutomotores {
 		
 	}
 	
-	public ClasseAutomotores(String marcaPar, String nomePar, String tipoPar, String corPar) {
+	public ClasseAutomotores(String corPar, String nomePar, String marcaPar, String tipoPar) {
 		
 		this.corAt = corPar;
 		this.nomeAt = nomePar;
 		this.marcaAt = marcaPar;
-		this.corAt = corPar;
+		this.tipoAt = tipoPar;
 		
 	}
+	
+	//geters
+	public String pegar_cor() {
+		return corAt;}
+		
+	public String pegar_nome() {
+		return nomeAt;}
+	
+	public String pegar_marca() {
+		return marcaAt;}
+	
+	public String pegar_tipo() {
+		return tipoAt;}
+	
+	//seters
+	public void alterar_cor(String corPar) {
+		this.corAt = corPar;}
+	
+	public void alterar_nome(String nomePar) {
+		this.nomeAt = nomePar;}
+	
+	public void alterar_marca(String marcaPar) {
+		this.marcaAt = marcaPar;}
+	
+	public void alterar_tipo(String tipoPar) {
+		this.tipoAt = tipoPar;}
 }
-
